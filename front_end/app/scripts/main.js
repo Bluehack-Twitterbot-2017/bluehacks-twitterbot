0d3bdef9beb5439452cd0bc8a41ccc9ed1b6fb7b
jordanngeorge <jordanngeorge@gmail.com>
2017-07-20 21:18:27 -0400

    create segmented control with action form

--- front_end/app/scripts/main.js
+++ front_end/app/scripts/main.js
@@ -115,3 +115,32 @@
 //     initInput()
 //   })
 // }())
+
+
+var checked1 = $('#option-1').is(':checked');
+var checked2 = $('#option-2').is(':checked');
+
+if (checked1 == 1) {
+    $('.Step_1_Content').show();
+    $('.Step_2_Content').hide();
+    $('.Step_3_Content').hide();
+} else if (checked2 == 1) {
+    $('.Step_2_Content').show();
+    $('.Step_1_Content').hide();
+    $('.Step_3_Content').hide();
+}
+
+$(".segmented-control").change(function () {
+    var checked1 = $('#option-1').is(':checked');
+    var checked2 = $('#option-2').is(':checked');
+
+    if (checked1 == 1) {
+        $('.Step_1_Content').show();
+        $('.Step_2_Content').hide();
+        $('.Step_3_Content').hide();
+    } else if (checked2 == 1) {
+        $('.Step_2_Content').show();
+        $('.Step_1_Content').hide();
+        $('.Step_3_Content').hide();
+    }
+});
